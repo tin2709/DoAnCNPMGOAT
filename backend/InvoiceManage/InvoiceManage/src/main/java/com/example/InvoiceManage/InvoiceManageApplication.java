84fb6adef1e582773ddcca25a84a11de5b298acc
tin2709 <trungtinpham336@gmail.com>
2025-03-27 21:56:26 +0700

    first commit

---
+++ backend/InvoiceManage/InvoiceManage/src/main/java/com/example/InvoiceManage/InvoiceManageApplication.java
@@ -0,0 +1,13 @@
+package com.example.InvoiceManage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvoiceManageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InvoiceManageApplication.class, args);
+	}
+
+}
---
+++ backend/InvoiceManage/InvoiceManage/src/test/java/com/example/InvoiceManage/InvoiceManageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.InvoiceManage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvoiceManageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
